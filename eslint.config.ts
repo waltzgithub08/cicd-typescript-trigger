258d3fc6c8b0aecacc442d4cd1d21e3187763856
waltzgithub08 <waltz@DESKTOP-5CESIIL.localdomain>
2025-09-21 22:39:22 +1000

    added linting checks

---
+++ eslint.config.ts
@@ -0,0 +1,19 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    ignores: [ "coverage/", "node_modules/" ]
+  },
+  { 
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+    plugins: { js }, 
+    extends: ["js/recommended"], 
+    languageOptions: { 
+      globals: { ...globals.browser, ...globals.node } 
+    } 
+  },
+  tseslint.configs.recommended,
+]);
